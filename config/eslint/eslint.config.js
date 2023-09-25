1971668970f05054960951b116a990c4c141e48d
Sebastian Knott <sebastian.knott@rewe-digital.com>
2023-09-25 11:29:40 +0200

    Remove need for tsconfig.json for eslint
    * tsconfig.json parsing not longer helpful

--- config/eslint/eslint.config.js
+++ config/eslint/eslint.config.js
@@ -13,8 +13,7 @@ module.exports = {
     ],
     parserOptions: {
         ecmaVersion: 2016,
-        sourceType: 'module',
-        project: './tsconfig.json',
+        sourceType: 'module'
     },
     overrides: [
         {
