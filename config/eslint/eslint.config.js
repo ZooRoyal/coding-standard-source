71b49508738a2655123ba7fa2b77399df6a778f0
Marcelle Hövelmanns <marcelle.hoevelmanns@rewe-digital.com>
2024-04-02 12:11:34 +0200

    added also the part-tag definition in the tsx-overrides

--- config/eslint/eslint.config.js
+++ config/eslint/eslint.config.js
@@ -86,6 +86,7 @@ module.exports = {
                             'widgetFieldIgnore',
                             'widgetFieldStore',
                             'widgetFieldHelpMessage',
+                            'part'
                         ],
                     },
                 ],
